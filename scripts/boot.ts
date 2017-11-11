142006d47f0494649ccce25d7bb71919065e0907
priyamatnani <matnanipriya@gmail.com>
2017-11-11 13:04:45 -0500

    initial structure

---
+++ scripts/boot.ts
@@ -0,0 +1,5 @@
+import { bootstrap }    from 'angular2/platform/browser'
+import { SearchComponent } from './search.component'
+
+bootstrap( SearchComponent );
+
---
+++ scripts/search.component.ts
@@ -0,0 +1,9 @@
+import { Component } from 'angular2/core' ;
+
+@Component({
+  selector : 'sp-search',
+  template : '<h1> hello </h1>',
+})
+
+export class SearchComponent;
+
