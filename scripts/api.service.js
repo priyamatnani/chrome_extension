944541ee0085c2d5a650991bbd2d9e131c8d49d5
priyamatnani <matnanipriya@gmail.com>
2017-11-11 14:56:32 -0500

    adding rest api files

---
+++ scripts/api.service.js
@@ -0,0 +1,44 @@
+System.register(['angular2/core', 'angular2/http', 'rxjs/add/operator/map'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, http_1;
+    var API_URL, ApiService;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (_1) {}],
+        execute: function() {
+            API_URL = "https://www.google.com";
+            ApiService = (function () {
+                function ApiService(http) {
+                    this.http = http;
+                }
+                ApiService.prototype.getSearchResults = function () {
+                    return this.http.get(API_URL)
+                        .map(function (res) { return res.text(); });
+                };
+                ApiService = __decorate([
+                    core_1.Injectable(), 
+                    __metadata('design:paramtypes', [http_1.Http])
+                ], ApiService);
+                return ApiService;
+            }());
+            exports_1("ApiService", ApiService);
+        }
+    }
+});
+//# sourceMappingURL=api.service.js.map
